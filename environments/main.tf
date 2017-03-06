e565d75367f4de84c69de8bbc5acdffdfd6911e1
Tom Miller <C-Thomas.X.Miller@charter.com>
2017-03-06 11:25:30 -0700

    Adding terraform version files and scratch environment.

--- environments/main.tf
+++ environments/main.tf
@@ -1,1 +1,18 @@
-# Create empty file
+#Configure provider
+provider "aws" {
+  region     = "${var.region}"
+  profile    = "${var.env}"
+}
+
+module "vpc" {
+    source = "github.com/terraform-community-modules/tf_aws_vpc"
+    name =  "${var.name}"
+    cidr =  "${var.cidr}"
+    azs = "${var.azs}"
+    private_subnets = "${var.private_subnets}"
+    public_subnets = "${var.public_subnets}"
+    enable_nat_gateway = "${var.enable_nat_gateway}"
+    enable_dns_support = "${var.enable_dns_support}"
+    enable_dns_hostnames = "${var.enable_dns_hostnames}"
+    tags {}
+}
---
+++ environments/scratch/scratch.tfvars
@@ -0,0 +1,9 @@
+env = "scratch"
+name = "EOS Scratch"
+
+azs = [ "us-west-1b" ]
+cidr = "10.0.0.0/16"
+instance_type = ""
+
+public_subnets = [ "10.0.1.0/24" ]
+private_subnets = [ "10.0.2.0/24" ]
