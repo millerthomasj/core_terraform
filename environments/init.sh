ace78c43b346581cd15aee143ccef567432a4d4e
Tom Miller <C-Thomas.X.Miller@charter.com>
2017-03-06 11:43:16 -0700

    Doing the branch system of deployment.

--- environments/init.sh
+++ environments/init.sh
@@ -7,8 +7,19 @@
 # terraform plan  -var-file=./production.tfvars 
 # terraform apply -var-file=./production.tfvars 
 
-tf_env="scratch"
+tf_env=$(printenv | grep ENVIRONMENT)
+
+if [ $? -ne 0 ] ; then
+   echo "The environment variable ENVIRONMENT is not set as one of the following:"
+   echo "   - scratch"
+   echo "   - development"
+   echo "   - staging"
+   echo "   - production"
+   exit 1
+fi
+
 region="us-west-1"
+
 
 terraform remote config -backend=s3 \
                         -backend-config="bucket=eos-terraform-state" \
--- environments/scratch/scratch.tfvars
+++
@@ -1,9 +0,0 @@
-env = "scratch"
-name = "EOS Scratch"
-
-azs = [ "us-west-1b" ]
-cidr = "10.0.0.0/16"
-instance_type = ""
-
-public_subnets = [ "10.0.1.0/24" ]
-private_subnets = [ "10.0.2.0/24" ]
