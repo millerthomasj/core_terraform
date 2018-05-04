1450769b4a3214738e57685d53fdf3d45bfa3cfe
Azamat Mukhiddinov <c-azamat.mukhiddinov@charter.com>
2018-05-04 08:59:43 -0600

    fix subnet lookups add security groups as data items

--- info.tf
+++ info.tf
@@ -2,7 +2,7 @@ data "aws_subnet_ids" "private_subnets" {
   vpc_id = "${var.vpc_id}"
 
   tags {
-    Name = "${var.public_subnet_filter}"
+    Name = "${var.private_subnet_filter}"
   }
 }
 
@@ -10,6 +10,19 @@ data "aws_subnet_ids" "public_subnets" {
   vpc_id = "${var.vpc_id}"
 
   tags {
-    Name = "${var.private_subnet_filter}"
+    Name = "${var.public_subnet_filter}"
   }
+}
+
+data "aws_security_group" "ssh_pa" {
+  name = "portals_${var.environment}_ssh_pa"
+}
+
+
+data "aws_security_group" "ssh_elb" {
+  name = "portals_${var.environment}_ssh_elb"
+}
+
+data "aws_security_group" "ssh_bastion" {
+  name = "portals_${var.environment}_ssh_bastion"
 }
