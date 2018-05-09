9c2a4888acb680f7388c6ad1b17073adf37dbb15
Azamat Mukhiddinov <c-azamat.mukhiddinov@charter.com>
2018-05-08 21:52:05 -0600

    add dns outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,22 @@ output "vpc_id" {
   value = "${var.vpc_id}"
 }
 
+output "route53_zone_local" {
+  value = "${data.aws_route53_zone.local.name}"
+}
+
+output "route53_zone_local_id" {
+  value = "${data.aws_route53_zone.local.zone_id}"
+}
+
+output "route53_zone_public" {
+  value = "${data.aws_route53_zone.public.name}"
+}
+
+output "route53_zone_public_id" {
+  value = "${data.aws_route53_zone.public.zone_id}"
+}
+
 output "private_subnet_ids" {
   value = "${data.aws_subnet_ids.private_subnets.ids}"
 }
