eff5479bd3b53081d6602b726fb86a3aa888e9e9
Tom Miller <tom.miller@charter.com>
2018-10-19 12:29:53 -0600

    Add a - at the end of the name prefix.

--- subnet_group.tf
+++ subnet_group.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "db_subnet_group" {	
-  name_prefix = "${var.project}-${var.env}"
+  name_prefix = "${var.project}-${var.env}-"
   subnet_ids  = ["${data.aws_subnet_ids.private_subnets.ids}"]	
 	
   tags {	
