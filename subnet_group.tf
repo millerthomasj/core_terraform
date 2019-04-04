9f21d4aad3a2acc7b57c8f0c8d726ebb4766db9d
Brian Johnson <C-Brian.Johnson2@charter.com>
2019-04-04 14:21:23 -0600

    switching from underscores to hyphens

--- subnet_group.tf
+++ subnet_group.tf
@@ -5,11 +5,11 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   tags {	
     Terraform = true
     Project   = "${var.project}"
-    Name      = "${var.project}-${var.env}-db_subnet_group"
+    Name      = "${var.project}-${var.env}-db-subnet-group"
   }
 }
 
 resource "aws_elasticache_subnet_group" "ec_subnet_group" {
-  name       = "${var.env}-${var.project}-ec_subnet_group"
+  name       = "${var.env}-${var.project}-ec-subnet-group"
   subnet_ids = ["${data.aws_subnet_ids.private_subnets.ids}"]
 }
