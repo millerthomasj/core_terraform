09219f97dc33682060fa8007be4e111d465ccb9b
Brian Johnson <C-Brian.Johnson2@charter.com>
2019-04-04 13:34:34 -0600

    removing tags

--- subnet_group.tf
+++ subnet_group.tf
@@ -12,10 +12,4 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 resource "aws_elasticache_subnet_group" "ec_subnet_group" {
   name       = "${var.env}-${var.project}-ec_subnet_group"
   subnet_ids = ["${data.aws_subnet_ids.private_subnets.ids}"]
-  
-  tags {	
-    Terraform = true
-    Project   = "${var.project}"
-    Name      = "${var.project}-${var.env}-ec_subnet_group"
-  }
 }
